9b320de0fe634db5b18894de0bba059aadcf5d53
ThanhNgo2k1 <ngotanthanh1342001@gmail.com>
2025-03-21 13:39:23 +0700

    test2
    test2

--- Droppii/src/App.jsx
+++ Droppii/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import ProductCard from "./Component/ProductCard";
+import ProductList from "./Component/ProductList";
+
 function App() {
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center p-10">
-        <ProductCard />
-      </div>
-    </>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-10">
+      <Routes>
+        <Route path="/" element={<ProductCard />} />
+        <Route path="/productlist" element={<ProductList />} />
+      </Routes>
+    </div>
   );
 }
 
